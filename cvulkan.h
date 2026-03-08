428f2199ba903225e06793c5a0f5d78437e1d778
botondmester <boboooka1@gmail.com>
2026-03-08 21:28:34 +0100

    make the window resizeable

--- cvulkan.h
+++ cvulkan.h
@@ -4,8 +4,10 @@
 
 #include <stdbool.h>
 
+// note: the caller is responsible for preserving the glfw window resize callback if they set one after initialization
+//       as cvk relies on the window resize callback
 bool cvk_init(GLFWwindow* window);
 
 void cvk_cleanup(void);
 
-void cvk_draw(void* image);
+void cvk_draw(GLFWwindow* window, void* image, size_t width, size_t height);
